1ed3431f3d86956a393ca14ba1584647a9c4c303
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-10-25 22:18:19 +0900

    init

---
+++ src/main/java/com/wapl/reidspubsub/ReidspubsubApplication.java
@@ -0,0 +1,13 @@
+package com.wapl.reidspubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidspubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReidspubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wapl/reidspubsub/ReidspubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wapl.reidspubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReidspubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
